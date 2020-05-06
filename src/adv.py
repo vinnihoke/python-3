3001bdd0d75d83a6cf6f31c2b30745310a32767d
Vinni Hoke <vinni.hoke@gmail.com>
2020-05-06 16:23:24 -0700

    Completed Day 1 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,19 +1,21 @@
 from room import Room
+from player import Player
+import random
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside': Room("Outside Cave Entrance",
+                    "North of you, the cave mouth beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer': Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow': Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
@@ -24,6 +26,7 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
+# Players room            Players new room
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -33,17 +36,76 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
 # Main
-#
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("Joe", room["outside"])
+
+
+def guess():
+    create = random.randrange(1, 5)
+    guess = input("🧐 Pick a number between 1 and 5: ")
+    if create == int(guess):
+        return True
+    else:
+        return False
+
 
 # Write a loop that:
+while True:
+    try:
+        # * Prints the current room name
+        print(f"\n\nCurrent room: {player.current_room.name}")
+
+        # * Prints the current description (the textwrap module might be useful here).
+        print(f"\nRoom description: {player.current_room.description}")
+
+        # * Waits for user input and decides what to do.
+        availableCommands = ["q - quit", "n - north",
+                             "s - south", "e - east", "w - west"]
+        print("\nAvailable commands are...")
+        for i in availableCommands:
+            print(i)
+
+        print("\n##############################################\n")
+        command = input("\nEnter your command: ")
+        print("\n##############################################\n")
+
+        if command == "q":
+            break
+
+        if command == "n":
+            if player.current_room.n_to == None:
+                print("\n\n🛑 Cannot advance")
+            else:
+                player.change_room(player.current_room.n_to)
+
+        if command == "s":
+            if player.current_room.name == "Narrow Passage" and player.current_room.s_to == None:
+                choice = guess()
+                if choice == True:
+                    print("\n\n🛑 Cannot advance")
+                else:
+                    print("\n\n👻 Run for your life and try again!")
+                    player.change_room(room["outside"])
+            else:
+                player.change_room(player.current_room.s_to)
+
+        if command == "e":
+            if player.current_room.e_to == None:
+                print("\n\n🛑 Cannot advance")
+            else:
+                player.change_room(player.current_room.e_to)
+
+        if command == "w":
+            if player.current_room.w_to == None:
+                print("\n\n🛑 Cannot advance")
+            else:
+                player.change_room(player.current_room.w_to)
+    except AttributeError:
+        player.change_room(room["outside"])
+
 #
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
---
+++ src/items.py
@@ -0,0 +1,12 @@
+import random
+
+# Type of item
+randomIndex = random.randrange(0, 4)
+
+# Rooms an array
+
+# Item is placed in random index in array
+
+# Item in backpack
+
+# Use item
